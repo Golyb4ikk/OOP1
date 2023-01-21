85d1c1d45743e8fd6f8872e5d8eccaa8406d3e1f
Golyb4ik <only_game2@mail.ru>
2023-01-21 22:07:46 +0300

    задание 3

--- src/Car.java
+++ src/Car.java
@@ -7,52 +7,27 @@ public class Car {
     private String country;
 
 
-    public String getBrand() {
-        return brand;
+   public Car(String brand,String model,double engineVolume,String color,int year,String country){
+       this.brand=validateParam(brand);
+       this.model=model;
+       this.engineVolume=engineVolume;
+       this.color=color;
+       this.year=valYear(year);
+       this.country=country;
+   }
+    public static Integer valYear(Integer value)
+    {return value == null?2000:value;}
+
+
+   public static String validateParam(String value) {
+        return Validation.validateString(value,"default");
     }
 
-    public void setBrand(String brand) {
-        this.brand = brand;
+    public static double validateEngineVolume(double value) {
+        return value<0 ?1.5 :value;
     }
-
-    public String getModel() {
-        return model;
-    }
-
-    public void setModel(String model) {
-        this.model = model;
-    }
-
-    public double getEngineVolume() {
-        return engineVolume;
-    }
-
-    public void setEngineVolume(double engineVolume) {
-        this.engineVolume = engineVolume;
-    }
-
-    public String getColor() {
-        return color;
-    }
-
-    public void setColor(String color) {
-        this.color = color;
-    }
-
-    public int getYear() {
-        return year;
-    }
-
-    public void setYear(int year) {
-        this.year = year;
-    }
-
-    public String getCountry() {
-        return country;
-    }
-
-    public void setCountry(String country) {
-        this.country = country;
+    public static String valColor(String value){
+        return Validation.validateString(value,"default");
     }
 
     @Override
@@ -65,4 +40,5 @@ public class Car {
                         ", year=" + year +
                         ", country='" + country + '\'';
     }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,49 +1,21 @@
 public class Main {
     public static void main(String[] args) {
 
-        Car car1 = new Car();
-        car1.setBrand("Lada");
-        car1.setColor("Yellow");
-        car1.setCountry("Russia");
-        car1.setModel("Granta");
-        car1.setEngineVolume(1.7);
-        car1.setYear(2015);
+
+        Car car1 = new Car("Lada","Granta",1.7,"Yellow",2015,"Russia");
+
+        Car car2 = new Car("Audi","A8",3.0,"Black",2020,"Germany");
+
+        Car car3 = new Car("Kia","Sport",2.4,"Red",2018,"East Corea");
+
+        Car car4 = new Car("Hyundai","Avante",1.6,"Orange",2016,"East Corea");
+
+        Car car5 = new Car("Bmw","z8",3.0,"Black",2021,"German");
+
         System.out.println(car1);
-
-        Car car2 = new Car();
-        car2.setBrand("Audi");
-        car2.setColor("Black");
-        car2.setCountry("Germany");
-        car2.setModel("A8");
-        car2.setEngineVolume(3.0);
-        car2.setYear(2020);
         System.out.println(car2);
-
-        Car car3 = new Car();
-        car3.setBrand("Kia");
-        car3.setColor("Red");
-        car3.setCountry("East Corea");
-        car3.setModel("Sport");
-        car3.setEngineVolume(2.4);
-        car3.setYear(2018);
         System.out.println(car3);
-
-        Car car4 = new Car();
-        car4.setBrand("Hyundai");
-        car4.setColor("Orange");
-        car4.setCountry("East Corea");
-        car4.setModel("Avante");
-        car4.setEngineVolume(1.6);
-        car4.setYear(2016);
         System.out.println(car4);
-
-        Car car5 = new Car();
-        car5.setBrand("Bmw");
-        car5.setColor("Black");
-        car5.setCountry("German");
-        car5.setModel("z8");
-        car5.setEngineVolume(3.0);
-        car5.setYear(2021);
         System.out.println(car5);
 
     }
